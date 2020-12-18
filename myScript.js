aeb567fc1c64895ca5386e3fb654cf129711f019
sumsar8 <rasmus.oberg@elev.ga.ntig.se>
2020-12-18 10:18:23 +0100

    iuujn

--- myScript.js
+++ myScript.js
@@ -9,6 +9,10 @@ function clickCounter() {
     document.getElementById("result").innerHTML = localStorage.clickcount;
 }
 
+    if (localStorage.knappuppgraderingar == 1)
+        document.getElementById("idforclickcounter").onclick = emilcounter4x;
+        document.getElementById("Upgradepic1").src = "/images/checkmark.png"
+        document.getElementById("cost10").innerHTML = ("Bought!");   
 
 function Upgradebuttona(){
     if (localStorage.clickcount > 9){
@@ -17,9 +21,10 @@ function Upgradebuttona(){
         localStorage.clickcount = Number(localStorage.clickcount) - 10;
         document.getElementById("f").src = "/images/emil2.jpg";
         document.getElementById("cost10").innerHTML = ("Bought!");
-            if (y == 0){
-                document.getElementById("piss").onclick = emilcounter4x;
-                y = 1;
+        
+        if (localStorage.knappuppgraderingar == 0){
+            document.getElementById("idforclickcounter").onclick = emilcounter4x;
+            localStorage.knappuppgraderingar = 1;
         }
         
     }
@@ -36,14 +41,15 @@ function Upgradebuttonb(){
         localStorage.clickcount = Number(localStorage.clickcount) - 100;
         document.getElementById("f").src = "/images/emil3.png";
         document.getElementById("cost100").innerHTML = ("Bought!");
-            if (y == 0, 1){
-                document.getElementById("f").onclick = emilcounter10x;
-                y = 2;
+        
+        if (localStorage.knappuppgraderingar == 0, 1){
+            document.getElementById("idforclickcounter").onclick = emilcounter10x;
+            localStorage.knappuppgraderingar = 2;
         }
     }
 }
 function emilcounter10x(){
-    localStorage.clickcount = Number(localStorage.clickcount) + 9;
+    localStorage.clickcount = Number(localStorage.clickcount) + 10;
     document.getElementById("result").innerHTML = localStorage.clickcount;
 }
 
@@ -54,13 +60,15 @@ function Upgradebuttonc(){
         localStorage.clickcount = Number(localStorage.clickcount) - 1000;
         document.getElementById("f").src = "/images/emil4.png";
         document.getElementById("cost1000").innerHTML = ("Bought!");
-            if (y == 0, 1, 2)
-                document.getElementById("f").onclick = emilcounter16x;
-                y = 3;
+        
+        if (localStorage.knappuppgraderingar == 0, 1, 2){
+            document.getElementById("idforclickcounter").onclick = emilcounter16x;
+            localStorage.knappuppgraderingar = 3;
+        }
     }
 }
 function emilcounter16x(){
-    localStorage.clickcount = Number(localStorage.clickcount) + 15;
+    localStorage.clickcount = Number(localStorage.clickcount) + 16;
     document.getElementById("result").innerHTML = localStorage.clickcount;
 }
 
@@ -71,13 +79,15 @@ function Upgradebuttond(){
         localStorage.clickcount = Number(localStorage.clickcount) - 10000;
         document.getElementById("f").src = "/images/emil5.png";
         document.getElementById("cost10000").innerHTML = ("Bought!");
-            if (y == 0, 1, 2, 3)
-                document.getElementById("f").onclick = emilcounter50x;
-                y = 4;
+        
+        if (localStorage.knappuppgraderingar == 0, 1, 2, 3){
+            document.getElementById("idforclickcounter").onclick = emilcounter50x;
+            localStorage.knappuppgraderingar = 4;
+        }
     }
 }
 function emilcounter50x(){
-    localStorage.clickcount = Number(localStorage.clickcount) + 49;
+    localStorage.clickcount = Number(localStorage.clickcount) + 50;
     document.getElementById("result").innerHTML = localStorage.clickcount;
 }
 
@@ -88,19 +98,24 @@ function Upgradebuttone(){
         localStorage.clickcount = Number(localStorage.clickcount) - 100000;
         document.getElementById("f").src = "/images/Windis.png";
         document.getElementById("cost100000").innerHTML = ("Bought!");
-            if (y == 0, 1, 2, 3, 4)
-                document.getElementById("f").onclick = emilcounter100x;
-                y = 5;
+        
+        if (localStorage.knappuppgraderingar == 0, 1, 2, 3, 4){
+            document.getElementById("idforclickcounter").onclick = emilcounter100x;
+            localStorage.knappuppgraderingar = 4;
+        }
     }
 }
 function emilcounter100x(){
-    localStorage.clickcount = Number(localStorage.clickcount) + 99;
+    localStorage.clickcount = Number(localStorage.clickcount) + 100;
     document.getElementById("result").innerHTML = localStorage.clickcount;
 }
 function resetlocalstorage(){
     localStorage.clickcount = 0;
     document.getElementById("result").innerHTML = localStorage.clickcount;
-    document.getElementById("f").onclick = clickCounter;
+    document.getElementById("idforclickcounter").onclick = clickCounter;
+    document.getElementById("f").src = "/images/emil1.jpg";
     localStorage.knappuppgraderingar = 0;
+    document.getElementById("Upgradepic1").src = "/images/MouseUpgrade2.png";
+    document.getElementById("cost10").innerHTML = ("Cost: 10");
 
 }
