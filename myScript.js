46e9ea54a774e25d34b46a6f52b1dacf58e1f26c
sumsar8 <60388964+sumsar8@users.noreply.github.com>
2020-12-21 21:39:30 +0100

    asdasd

--- myScript.js
+++ myScript.js
@@ -1,6 +1,10 @@
 var x, y, emilcoins;
 y = 0;
 
+setInterval(() => {
+    penisse()
+}, 1000);
+
 document.getElementById("result").innerHTML = localStorage.clickcount;
 
 function clickCounter() {
@@ -8,75 +12,11 @@ function clickCounter() {
     document.getElementById("result").innerHTML = localStorage.clickcount;
 }
 
-if (localStorage.knappuppgraderingar == 1) {
-    document.getElementById("idforclickcounter").onclick = emilcounter4x;
-    document.getElementById("Upgradepic1").src = "images/checkmark.png"
-    document.getElementById("cost10").innerHTML = ("Bought!");
-    document.getElementById("f").src = "images/emil2.jpg"
-    document.getElementById("upgradebutton1").onclick = shitclick;
+function penisse() {
+    localStorage.clickcount = Number(localStorage.clickcount) + 1;
+    document.getElementById("result").innerHTML = localStorage.clickcount;
 }
 
-if (localStorage.knappuppgraderingar == 2) {
-    document.getElementById("idforclickcounter").onclick = emilcounter10x;
-    document.getElementById("Upgradepic1").src = "images/checkmark.png"
-    document.getElementById("cost10").innerHTML = ("Bought!");
-    document.getElementById("Upgradepic2").src = "images/checkmark.png"
-    document.getElementById("cost100").innerHTML = ("Bought!");
-    document.getElementById("f").src = "images/emil3.png" 
-    document.getElementById("upgradebutton1").onclick = shitclick;
-    document.getElementById("upgradebutton2").onclick = shitclick;
-}
-
-if (localStorage.knappuppgraderingar == 3) {
-    document.getElementById("idforclickcounter").onclick = emilcounter16x;
-    document.getElementById("Upgradepic1").src = "images/checkmark.png"
-    document.getElementById("cost10").innerHTML = ("Bought!");
-    document.getElementById("Upgradepic2").src = "images/checkmark.png"
-    document.getElementById("cost100").innerHTML = ("Bought!");
-    document.getElementById("Upgradepic3").src = "images/checkmark.png"
-    document.getElementById("cost1000").innerHTML = ("Bought!");
-    document.getElementById("f").src = "images/emil4.png" 
-    document.getElementById("upgradebutton1").onclick = shitclick;
-    document.getElementById("upgradebutton2").onclick = shitclick;
-    document.getElementById("upgradebutton3").onclick = shitclick;
-}
-
-if (localStorage.knappuppgraderingar == 4) {
-    document.getElementById("idforclickcounter").onclick = emilcounter50x;
-    document.getElementById("Upgradepic1").src = "images/checkmark.png"
-    document.getElementById("cost10").innerHTML = ("Bought!");
-    document.getElementById("Upgradepic2").src = "images/checkmark.png"
-    document.getElementById("cost100").innerHTML = ("Bought!");
-    document.getElementById("Upgradepic3").src = "images/checkmark.png"
-    document.getElementById("cost1000").innerHTML = ("Bought!");
-    document.getElementById("Upgradepic4").src = "images/checkmark.png"
-    document.getElementById("cost10000").innerHTML = ("Bought!");
-    document.getElementById("f").src = "images/emil5.png" 
-    document.getElementById("upgradebutton1").onclick = shitclick;
-    document.getElementById("upgradebutton2").onclick = shitclick;
-    document.getElementById("upgradebutton3").onclick = shitclick;
-    document.getElementById("upgradebutton4").onclick = shitclick;
-}
-
-if (localStorage.knappuppgraderingar == 5) {
-    document.getElementById("idforclickcounter").onclick = emilcounter100x;
-    document.getElementById("Upgradepic1").src = "images/checkmark.png"
-    document.getElementById("cost10").innerHTML = ("Bought!");
-    document.getElementById("Upgradepic2").src = "images/checkmark.png"
-    document.getElementById("cost100").innerHTML = ("Bought!");
-    document.getElementById("Upgradepic3").src = "images/checkmark.png"
-    document.getElementById("cost1000").innerHTML = ("Bought!");
-    document.getElementById("Upgradepic4").src = "images/checkmark.png"
-    document.getElementById("cost10000").innerHTML = ("Bought!");
-    document.getElementById("Upgradepic5").src = "images/checkmark.png"
-    document.getElementById("cost100000").innerHTML = ("Bought!");
-    document.getElementById("f").src = "images/Windis.png"
-    document.getElementById("upgradebutton1").onclick = shitclick;
-    document.getElementById("upgradebutton2").onclick = shitclick;
-    document.getElementById("upgradebutton3").onclick = shitclick;
-    document.getElementById("upgradebutton4").onclick = shitclick;
-    document.getElementById("upgradebutton5").onclick = shitclick;
-}
 function Upgradebuttona(){
     if (localStorage.clickcount > 9){
         document.getElementById("Upgradepic1").src = "images/checkmark.png";
@@ -193,4 +133,75 @@ function resetlocalstorage(){
     document.getElementById("cost10000").innerHTML = ("Cost: 10 000");
     document.getElementById("Upgradepic5").src = "images/MouseUpgrade6.png";
     document.getElementById("cost100000").innerHTML = ("Cost: 100 000");
+}
+
+
+if (localStorage.knappuppgraderingar == 1) {
+    document.getElementById("idforclickcounter").onclick = emilcounter4x;
+    document.getElementById("Upgradepic1").src = "images/checkmark.png"
+    document.getElementById("cost10").innerHTML = ("Bought!");
+    document.getElementById("f").src = "images/emil2.jpg"
+    document.getElementById("upgradebutton1").onclick = shitclick;
+}
+
+if (localStorage.knappuppgraderingar == 2) {
+    document.getElementById("idforclickcounter").onclick = emilcounter10x;
+    document.getElementById("Upgradepic1").src = "images/checkmark.png"
+    document.getElementById("cost10").innerHTML = ("Bought!");
+    document.getElementById("Upgradepic2").src = "images/checkmark.png"
+    document.getElementById("cost100").innerHTML = ("Bought!");
+    document.getElementById("f").src = "images/emil3.png" 
+    document.getElementById("upgradebutton1").onclick = shitclick;
+    document.getElementById("upgradebutton2").onclick = shitclick;
+}
+
+if (localStorage.knappuppgraderingar == 3) {
+    document.getElementById("idforclickcounter").onclick = emilcounter16x;
+    document.getElementById("Upgradepic1").src = "images/checkmark.png"
+    document.getElementById("cost10").innerHTML = ("Bought!");
+    document.getElementById("Upgradepic2").src = "images/checkmark.png"
+    document.getElementById("cost100").innerHTML = ("Bought!");
+    document.getElementById("Upgradepic3").src = "images/checkmark.png"
+    document.getElementById("cost1000").innerHTML = ("Bought!");
+    document.getElementById("f").src = "images/emil4.png" 
+    document.getElementById("upgradebutton1").onclick = shitclick;
+    document.getElementById("upgradebutton2").onclick = shitclick;
+    document.getElementById("upgradebutton3").onclick = shitclick;
+}
+
+if (localStorage.knappuppgraderingar == 4) {
+    document.getElementById("idforclickcounter").onclick = emilcounter50x;
+    document.getElementById("Upgradepic1").src = "images/checkmark.png"
+    document.getElementById("cost10").innerHTML = ("Bought!");
+    document.getElementById("Upgradepic2").src = "images/checkmark.png"
+    document.getElementById("cost100").innerHTML = ("Bought!");
+    document.getElementById("Upgradepic3").src = "images/checkmark.png"
+    document.getElementById("cost1000").innerHTML = ("Bought!");
+    document.getElementById("Upgradepic4").src = "images/checkmark.png"
+    document.getElementById("cost10000").innerHTML = ("Bought!");
+    document.getElementById("f").src = "images/emil5.png" 
+    document.getElementById("upgradebutton1").onclick = shitclick;
+    document.getElementById("upgradebutton2").onclick = shitclick;
+    document.getElementById("upgradebutton3").onclick = shitclick;
+    document.getElementById("upgradebutton4").onclick = shitclick;
+}
+
+if (localStorage.knappuppgraderingar == 5) {
+    document.getElementById("idforclickcounter").onclick = emilcounter100x;
+    document.getElementById("Upgradepic1").src = "images/checkmark.png"
+    document.getElementById("cost10").innerHTML = ("Bought!");
+    document.getElementById("Upgradepic2").src = "images/checkmark.png"
+    document.getElementById("cost100").innerHTML = ("Bought!");
+    document.getElementById("Upgradepic3").src = "images/checkmark.png"
+    document.getElementById("cost1000").innerHTML = ("Bought!");
+    document.getElementById("Upgradepic4").src = "images/checkmark.png"
+    document.getElementById("cost10000").innerHTML = ("Bought!");
+    document.getElementById("Upgradepic5").src = "images/checkmark.png"
+    document.getElementById("cost100000").innerHTML = ("Bought!");
+    document.getElementById("f").src = "images/Windis.png"
+    document.getElementById("upgradebutton1").onclick = shitclick;
+    document.getElementById("upgradebutton2").onclick = shitclick;
+    document.getElementById("upgradebutton3").onclick = shitclick;
+    document.getElementById("upgradebutton4").onclick = shitclick;
+    document.getElementById("upgradebutton5").onclick = shitclick;
 }
